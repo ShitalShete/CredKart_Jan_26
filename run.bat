91b0c7ee975adb82c5825bcbddee5aeb911f28b1
ShitalShete <shitalshtete55428@gmail.com>
2026-02-05 16:30:07 +0530

    edited

--- run.bat
+++ run.bat
@@ -1,2 +1,3 @@
 pytest -n=auto --html=Html_Reports\my_chromereport_28_jan_2026.html --browser chrome
 pytest -n=auto --html=Html_Reports\my_chromereport_28_jan_2026.html --browser firefox
+pytest -n=auto --html=Html_Reports\my_chromereport_28_jan_2026.html --browser edge
